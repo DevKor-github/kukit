c6a8c34593d0c85b6e5a17e9b6b22de1e4bf801e
overthestream <njhnjh02@naver.com>
2024-04-29 17:16:18 +0900

    fix login request headers

--- token.ts
+++ token.ts
@@ -2,18 +2,26 @@ const firstDelimiter = `<input type="password" name="pw" id="_pw" value="" />`;
 const secondDelimiter = `<input type="hidden" name="direct_div"/>`;
 
 export async function getToken(id: string, password: string): Promise<string> {
-  const { id: idKey, password: passwordKey, csrf, fake } = await (async () => {
-    const res = await fetch(
-      "https://portal.korea.ac.kr/front/Intro.kpd",
-      { headers: { referer: "https://portal.korea.ac.kr/front/Intro.kpd" } },
-    );
+  const {
+    id: idKey,
+    password: passwordKey,
+    csrf,
+    fake,
+    sessionId,
+  } = await (async () => {
+    const res = await fetch("https://portal.korea.ac.kr/front/Intro.kpd", {
+      headers: { referer: "https://portal.korea.ac.kr/front/Intro.kpd" },
+    });
     if (!res.ok) throw Error("Failed to fetch kupid/intro");
     const text = await res.text();
     const [_, b] = text.split(firstDelimiter);
     if (!b) throw Error("Failed to parse kupid/intro @ 1st delimiter");
     const [c, d] = b.split(secondDelimiter);
     if (!d) throw Error("Failed to parse kupid/intro @ 2nd delimiter");
-    const elems = c.split("\n").map((line) => line.trim()).filter(Boolean);
+    const elems = c
+      .split("\n")
+      .map((line) => line.trim())
+      .filter(Boolean);
     if (elems.length !== 4) {
       throw Error("Failed to parse kupid/intfo @ elem trimming");
     }
@@ -30,10 +38,15 @@ export async function getToken(id: string, password: string): Promise<string> {
     const [, password] = passwordMatchArray;
     const [, csrf] = csrfMatchArray;
     const [, fake] = fakeElemMatchArray;
+    const cookies = res.headers.getSetCookie();
+    if (!cookies.length) throw Error("Unexpected length of set-cookie");
+    const parsed = cookies.map(parseSetCookie);
+    const sessionId = parsed.find(({ key }) => key === "PORTAL_SESSIONID");
+    if (!sessionId) throw Error("Failed to get sessionId from set-cookie result");
     if (!id || !password || !csrf || !fake) {
       throw Error("Failed to parse elements");
     }
-    return { id, password, csrf, fake };
+    return { id, password, csrf, fake, sessionId: sessionId.value };
   })();
 
   const token = await (async () => {
@@ -47,14 +60,17 @@ export async function getToken(id: string, password: string): Promise<string> {
       browser: "chrome",
     });
     const res = await fetch("https://portal.korea.ac.kr/common/Login.kpd", {
-      headers: { referer: "https://portal.korea.ac.kr/front/Intro.kpd" },
+      headers: {
+        referer: "https://portal.korea.ac.kr/front/Intro.kpd",
+        "Content-Type": "application/x-www-form-urlencoded",
+        Cookie: `PORTAL_SESSIONID=${sessionId}`,
+      },
       body,
       method: "POST",
       redirect: "manual",
     });
     if (res.status !== 302) throw Error("Failed to kupid/login");
     const cookies = res.headers.getSetCookie();
-    console.log(cookies);
     if (!cookies.length) throw Error("Unexpected length of set-cookie");
     const parsed = cookies.map(parseSetCookie);
     const token = parsed.find(({ key }) => key === "ssotoken");
