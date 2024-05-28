64f85da2d8af833b255df107f25d8b9b4c62d56f
Park SeungHyun <danbaram0420@korea.ac.kr>
2024-05-28 19:36:32 +0900

    feat/오타 수정

--- type-a/me.ts
+++ type-a/me.ts
@@ -1,30 +1,67 @@
 import {
-  getContent,
-  getDate,
-  getTitle,
+  convertRelativeFilePath,
+  fetchWithError,
   getTypeAUrlList,
   getWriter,
-  makeFilePathPublic,
-  type typeANotice,
+  parseArticle,
+  type TypeANotice,
 } from "./type-a.ts";
 
+enum MeCategory {
+  UnderAndGrad = 0,
+  Undergraduate = 1,
+  Graduate = 2,
+  Job = 3,
+}
+
 /**
- * @param page 탐색할 페이지 번호
- * @param category 탐색할 카테고리 번호, 1: 학부, 2: 대학원, 3: 취업정보
+ * @param listnum 탐색할 게시글 최대치
+ * @param category 탐색할 카테고리 번호, 0: 일반(학부 및 대학원, 취업정보는 해당없음), 1: 학부, 2: 대학원, 3: 취업정보
  * @returns 해당 페이지의 공지사항 URL 리스트를 반환합니다.
  */
-export async function getEeUrlList(
-  page: number,
-  category: number,
+export async function getMeUrlList(
+  listnum: number,
+  category: MeCategory,
 ): Promise<string[]> {
-  const eeCategory = {
-    1: "undernotice",
-    2: "undernotice",
-    3: "job",
-  }[category];
-  if (!eeCategory) throw Error("Invalid category number");
-  const mainUrl =
-    `https://ee.korea.ac.kr/community/${eeCategory}.html?&page=${page}`;
-  const hrefList = await getTypeAUrlList(mainUrl, `${eeCategory}_view`);
-  return hrefList.map((x) => "https://ee.korea.ac.kr/community/" + x);
+  const meCategory = (category != 3) ? "undernotice" : "job";
+  if (!meCategory) throw Error("Invalid category number");
+  const mainUrl = (category != 3)
+    ? `https://me.korea.ac.kr/community/undernotice.html?cate%5B%5D=${category}&listnum=${listnum}`
+    : `https://me.korea.ac.kr/community/job.html?listnum=${listnum}`;
+
+  const hrefList = await getTypeAUrlList(mainUrl, `${meCategory}_view`);
+  return hrefList.map((x) => "https://me.korea.ac.kr/community/" + x);
+}
+
+/**
+ * @param url getMeUrlList 함수에서 반환된 url 하나
+ * @param mainCategory 탐색중인 카테고리 번호, 0: 일반(학부 및 대학원, 취업정보는 해당없음), 1: 학부, 2: 대학원, 3: 취업정보
+ * @returns 공지사항의 제목, 작성자, 게시일자, public URL, HTML table body, 카테고리 내용을 반환합니다.
+ */
+export async function getNoticeFromMe(
+  url: string,
+  mainCategory: MeCategory,
+): Promise<TypeANotice> {
+  const scrap = await fetchWithError(url);
+  const html = await scrap.text();
+  const article = convertRelativeFilePath(html, "https://me.korea.ac.kr");
+
+  const noticeData = parseArticle(article, { title: "div", date: "p" });
+  noticeData.url = url;
+
+  const subcategory = {
+    1: "학부",
+    2: "대학원",
+    3: "취업정보",
+  }[mainCategory];
+  if (!subcategory) throw Error("Invalid category number");
+
+  noticeData.writer = getWriter(article, "name");
+
+  noticeData.category = subcategory +
+    (mainCategory == 3
+      ? ""
+      : (noticeData.title.includes("장학") ? " 장학" : " 공지"));
+
+  return noticeData;
 }
