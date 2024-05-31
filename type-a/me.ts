2860bc03662cc581da3e86616ae76072d61b16ab
danbaram0420 <68544417+danbaram0420@users.noreply.github.com>
2024-05-31 19:25:28 +0900

    Update me.ts

--- type-a/me.ts
+++ type-a/me.ts
@@ -36,8 +36,8 @@ export async function getMeUrlList(
  * @param url getMeUrlList 함수에서 반환된 url 하나
  * @param mainCategory 탐색중인 카테고리 번호, 0: 일반(학부 및 대학원, 취업정보는 해당없음), 1: 학부, 2: 대학원, 3: 취업정보
  * @returns 공지사항의 제목, 작성자, 게시일자, public URL, HTML table body, 카테고리 내용을 반환합니다.
+ * 이 함수를 부를 때 순서는 1,2->0,3 추천. 일반은 취업정보 제외한 모든 게시글을 불러와 카테고리 구분이 안되기 때문.
  */
-//이 함수를 부를 때 순서는 1,2->0,3 추천. 일반은 취업정보 제외한 모든 게시글을 불러오기 때문.
 export async function getNoticeFromMe(
   url: string,
   mainCategory: MeCategory,
@@ -57,8 +57,8 @@ export async function getNoticeFromMe(
   }[mainCategory];
   if (!subcategory) throw Error("Invalid category number");
 
-  noticeData.writer = subcategory;
-  //기계공학부는 writer데이터가 없다
+  noticeData.writer = "기계공학부";
+  //기계공학부는 writer데이터가 없어서 통일
 
   noticeData.category = subcategory +
     (noticeData.title.includes("장학") ? " 장학" : " 공지");
